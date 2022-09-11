2ee9bda91d570fb68504c78db2bcd3ffa13f1be0
danilkk <ezzfvkoko@gmail.com>
2022-09-11 16:33:57 +0300

    я дурашка балвашка!! я в дравпанельке сделал наследование от джифрейм а не от джипанель!! вот я глупый!!!

--- src/ru/vsu/edu/shlyikov_d_g/DrawPanel.java
+++ src/ru/vsu/edu/shlyikov_d_g/DrawPanel.java
@@ -3,12 +3,15 @@ package ru.vsu.edu.shlyikov_d_g;
 import javax.swing.*;
 import java.awt.*;
 
-public class DrawPanel extends JFrame {
+public class DrawPanel extends JPanel {
     public DrawPanel() {
     }
 
     @Override
-    public void paint(Graphics g) {
-        super.paint(g);
+    public void paint(Graphics gr) {
+        super.paint(gr);
+        Graphics2D g = (Graphics2D) gr;
+
+        g.drawRect(1080, 444, 20, 100);
     }
 }
