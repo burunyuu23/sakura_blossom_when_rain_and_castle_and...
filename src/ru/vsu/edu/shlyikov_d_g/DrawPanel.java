87c89615cca570fe19a50228ff29b5bfd1f6e976
danilkk <ezzfvkoko@gmail.com>
2022-09-11 16:27:23 +0300

    сделал какую то дравпанельку для рисования последующего! очень рад

---
+++ src/ru/vsu/edu/shlyikov_d_g/DrawPanel.java
@@ -0,0 +1,14 @@
+package ru.vsu.edu.shlyikov_d_g;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DrawPanel extends JFrame {
+    public DrawPanel() {
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+    }
+}
--- src/ru/vsu/edu/shlyikov_d_g/Window.java
+++ src/ru/vsu/edu/shlyikov_d_g/Window.java
@@ -1,6 +1,13 @@
 package ru.vsu.edu.shlyikov_d_g;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame {
+    private final DrawPanel dp;
+
+    public Window() throws HeadlessException {
+        dp = new DrawPanel();
+        this.add(dp);
+    }
 }
